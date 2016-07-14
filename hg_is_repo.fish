d053001112e0696646ba985a4d520bef2c91258a
Gabriel Carneiro <gabtemp02@yahoo.com.br>
2016-07-13 23:23:38 -0300

    Optimizing hg root command and removing unnecessary dependency.

--- hg_is_repo.fish
+++ hg_is_repo.fish
@@ -1,5 +1,5 @@
 function hg_is_repo -d "Test if the current directory is a HG repository"
-    if not lookup .hg > /dev/null ^ /dev/null
+    if not hg_repository_root > /dev/null ^ /dev/null
         return 1
     end
 end
--- hg_repository_root.fish
+++ hg_repository_root.fish
@@ -1,5 +1,5 @@
 function hg_repository_root -d "Get the top level directory of the current hg repository"
-	set -l repo_root (lookup .hg) 
+	set -l repo_root (command hg root ^ /dev/null) 
     if test $status -ne 0
         return 1
     end
