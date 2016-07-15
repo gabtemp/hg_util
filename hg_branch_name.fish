b097b8ad6c1247ea3a5a91290705c3c9ae952cc9
Gabriel Carneiro <gabtemp02@yahoo.com.br>
2016-07-14 23:27:14 -0300

    Checking for hg repo before branch name

--- hg_branch_name.fish
+++ hg_branch_name.fish
@@ -1,6 +1,9 @@
 function hg_branch_name -d "Get the name of the current HG branch"
-    set -l branch_name (command hg branch)
+    if not hg_is_repo
+        return 1
+    end
 
+    set -l branch_name (command hg branch)
     if test -n "$branch_name"
         printf "%s\n" "$branch_name"
     end
--- hg_is_empty.fish
+++ hg_is_empty.fish
@@ -2,5 +2,6 @@ function hg_is_empty -d "Test if a repository is empty"
     if not hg_is_repo
         return 1
     end
+    
     return (command hg log --limit 1 --template "{node}\n" ^ /dev/null | wc --lines)
 end
