5d25e1be263127c6ad93165b5e0647794f0a485d
Gabriel Carneiro <gabtemp02@yahoo.com.br>
2016-07-16 15:42:43 -0300

    Printing the hg tag on the hg_branch_name function

--- hg_branch_name.fish
+++ hg_branch_name.fish
@@ -4,7 +4,20 @@ function hg_branch_name -d "Get the name of the current HG branch"
     end
 
     set -l branch_name (command hg branch)
+    
     if test -n "$branch_name"
-        printf "%s\n" "$branch_name"
+    	set -l tag_name (command hg log -r "." --template "{tags}")
+    	
+
+    	if test -z "$tag_name"
+    	   or test "tip" = "$tag_name"
+    		printf "%s\n" "$branch_name"
+    	else
+    		if test "default" = "$branch_name"
+    			printf "%s\n" "$tag_name"
+    		else
+				printf "%s|%s\n" "$branch_name" "$tag_name"
+			end
+    	end
     end
 end
--- test/hg_branch_name.fish
+++ test/hg_branch_name.fish
@@ -1,23 +1,47 @@
 set -l path $DIRNAME/.t-$TESTNAME-(random)
 
 function -S setup
-    mkdir -p $path/{default,branch}
+    mkdir -p $path/{default,branch,defaulttag,branchtag}
 
-    for name in default branch
+    for name in default branch defaulttag branchtag
     	cd $path/$name
         command hg --quiet init
-        command hg --quiet branch $name
+
+        if test "$name" = "branch"
+           or test "$name" = "branchtag"
+	    	command hg --quiet branch $name
+	    end
+
+        touch file
+    	command hg --quiet add file
+    	command hg --quiet ci -m "file" -u "Test"
     end
+	
+	cd $path/defaulttag
+    command hg --quiet tag tag
+    command hg up --quiet tag
+    
+    cd $path/branchtag
+    command hg --quiet tag tag
+    command hg up --quiet tag
 end
 
 function -S teardown
     rm -rf $path
 end
 
-test "$TESTNAME - Get the name of the current (default) HG branch"
+test "$TESTNAME - Get the name of the current (default) HG branch with no tag (tip)"
     "default" = (cd $path/default; hg_branch_name)
 end
 
-test "$TESTNAME - Get the name of the current HG branch"
+test "$TESTNAME - Get the name of the current HG branch with no tag (tip)"
     "branch" = (cd $path/branch; hg_branch_name)
+end
+
+test "$TESTNAME - Get the name of the current tag with no with no branch (default)"
+    "tag" = (cd $path/defaulttag; hg_branch_name)
+end
+
+test "$TESTNAME - Get the name of the current HG branch and tag"
+    "branchtag|tag" = (cd $path/branchtag; hg_branch_name)
 end
